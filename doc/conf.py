b198d48cf840498a62d65a4b30a2c158dd5507c3
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2022-10-04 12:12:35 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ exclude_patterns = [
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_logo = "img/UU_logo_color.svg"
+html_logo = "img/UU-logo.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
