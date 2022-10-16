e4106b5ca84ecc41dccd65897a5b7d1c06770f15
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2022-10-16 10:38:54 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('exts'))
+sys.path.insert(0, os.path.abspath('exts'))
 
 
 # -- Project information -----------------------------------------------------
